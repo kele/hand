217f84b20b90aa01eddbe8bda2d833ff9c75c41e
kele <damian.bogel@gmail.com>
2023-11-21 20:45:09 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,36 @@
+# Could You Give Your Program a Helping Hand From Time to Time?
+
+Imagine you process lots of data. From time to time, though, the data entries
+are a bit tricky to handle. Writing code that handles them would require a lot
+of effort... but you could **easily** handle these individual cases yourself,
+because you are a human.
+
+The only thing you need to do to enable this is to change your code from looking
+like so:
+
+```
+if v, err := Convert("shirt mint"); err != nil {
+    return nil, err
+} 
+process(v)
+```
+
+to
+
+```
+if v, err := hand.HelpWith(Convert)("shirt mint"); err != nil {
+    return nil, err
+} 
+process(v)
+```
+
+If the `Convert()` function were to return an error, a prompt in the terminal
+like so appears:
+
+```
+hand: /home/kele/hand/hand.go:20 -- f([shirt mint]) = (_, unknown color category).
+hand: Fix?
+```
+
+and you are expected to supply a JSON value and send `EndOfFile` (Ctrl+D in most
+terminals).
--- hand.go
+++ hand.go
@@ -8,6 +8,26 @@ import (
 	"runtime"
 )
 
+// HelpWith helps recover from errors.
+// TODO(kele): Add HelpWith2, HelpWith3, HelpWith4… for functions with more arguments.
+func HelpWith[Arg1 any, Result any](f func(Arg1) (Result, error)) func(Arg1) (Result, error) {
+	return func(arg1 Arg1) (Result, error) {
+		v, fErr := f(arg1)
+		if fErr == nil {
+			return v, nil
+		}
+		var ret Result
+		if err := Prompt(fErr, arg1); err != nil {
+			return ret, fmt.Errorf("hand.Prompt() = %v; original error: %w", err, fErr)
+		}
+		if err := GetAnswer(&ret); IsAnAnswer(ret, err) {
+			return ret, nil
+		} else {
+			return ret, fmt.Errorf("hand.IsAnAnswer() = false, hand.GetAnswer() = %v; original error: %w", err, fErr)
+		}
+	}
+}
+
 // Prompt is called after the function supplied to HelpWith returns an error.
 var Prompt = func(fErr error, args ...any) error {
 	_, file, line, _ := runtime.Caller(1)
@@ -29,23 +49,4 @@ var GetAnswer = func(object any) error {
 // considered as one (cf. treated as "I don't know the answer").
 var IsAnAnswer = func(object any, err error) bool {
 	return err == nil
-}
-
-// HelpWith helps recover from errors.
-func HelpWith[Arg1 any, Result any](f func(Arg1) (Result, error)) func(Arg1) (Result, error) {
-	return func(arg1 Arg1) (Result, error) {
-		v, fErr := f(arg1)
-		if fErr == nil {
-			return v, nil
-		}
-		var ret Result
-		if err := Prompt(fErr, arg1); err != nil {
-			return ret, fmt.Errorf("hand.Prompt() = %v; original error: %w", err, fErr)
-		}
-		if err := GetAnswer(&ret); IsAnAnswer(ret, err) {
-			return ret, nil
-		} else {
-			return ret, fmt.Errorf("hand.IsAnAnswer() = false, hand.GetAnswer() = %v; original error: %w", err, fErr)
-		}
-	}
 }
